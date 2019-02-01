9f94768223b57618737536b25458ab7aa89e9871
dmitry.dyachenko.work@gmail.com <dmitry.dyachenko.work@gmail.com>
2019-02-01 23:06:19 +0200

    add scrollUp

---
+++ Blog/Blog.WebUI/Scripts/scrollUp.js
@@ -0,0 +1,12 @@
+﻿window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scrollUp").style.display = "block";
+    } else {
+        document.getElementById("scrollUp").style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+}
